8fc46d3ac1e1b58f4f4a2923b0b233e5564faaf5
Peter Shen <petershen0307@hotmail.com>
2017-09-05 15:30:25 +0800

    remove printing result to console

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/petershen0307/getSHA2/cli"
-	"github.com/petershen0307/getSHA2/core"
 )
 
 func main() {
@@ -13,8 +11,8 @@ func main() {
 	app.Run(os.Args)
 	// core.SetFilter([]string{"C:\\Users\\PC\\Desktop\\code\\go\\src\\github.com\\petershen0307\\getSHA2\\.git"}, []string{})
 	// core.Start("C:\\Users\\PC\\Desktop\\code\\go\\src\\github.com\\petershen0307\\getSHA2")
-	out := core.GetOutputHash()
-	for key, value := range out {
-		log.Println("key:", key, ", value:", value)
-	}
+	// out := core.GetOutputHash()
+	// for key, value := range out {
+	// 	log.Println("key:", key, ", value:", value)
+	// }
 }
